fc3a3b3f1285beb35a1a3ae22a33c69fb00f8acb
sreyniet.loeurn <sreyniet.loeurn@pnc.lan>
2019-12-24 07:36:47 +0700

    project jquery

--- js/Jquery_Project.js
+++ js/Jquery_Project.js
@@ -1,63 +1,126 @@
 
-
+// Get URL
 
 function getUrl() {
-    var url ="https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
-$(document).ready(() =>{
+$(document).ready(() => {
     requestApi();
+
+// Hid button and input
+    $('#increaseDecreaseGuest').hide();
+
+
     $('#line').hide();
-    $('#recipe').on('change',function(){
+    $('#card-form').hide();
+    $('.big-quote').show();
+    // select change
+    $('#recipe').on('change', function () {
         $('#line').show();
+        $('#card-form').show();
+        $('.big-quote').hide();
+        $('#increaseDecreaseGuest').show();
         var recipes = $('#recipe').val();
         getRecipe(recipes);
        
-        
+       
+
+
     });
+    // Click add number of guests
+
+    $('#add').on('click',function() {
+        var add = $('#member').val();
+        addPerson(add);
+        
+    })
+
+    // Click to decrease number of guests
+    $('#decrease').on('click',function() {
+        var decrease = $('#member').val();
+        decreasePerson(decrease);
+        
+    })
 });
-function requestApi(){
+
+// Calculate quntities when increase or decrease guest
+    function calculate(calculate) {
+        var newQuantity;
+        var quantities;
+        var result;
+        oldQuantity.ingredients.forEach(items => {
+            quantities = items.quantity/oldGuest;
+            newQuantity = quantities*calculate
+            result +=`
+            <tr>
+            <td><img src="${items.iconUrl}" width="100"></td>
+             
+              <td id="quantity">${newQuantity}</td>
+              <td>${items.unit[0]}</td>
+              <td>${items.name}</td>
+              
+          </tr>
+            
+            `;
+            $('#ingredient').html(result);
+        })
+    }
+
+// Request data to store in data
+function requestApi() {
     $.ajax({
         dataType: 'json',
-        url:getUrl(),
+        url: getUrl(),
         success: (data) => chhosenRechipe(data.recipes),
         error: () => console.log("Cannot get data"),
     })
 }
+
+// To get name of food in to select form
 var alldata = [];
-function chhosenRechipe(rechipe){
+function chhosenRechipe(rechipe) {
     alldata = rechipe;
-    var Option ="";
-    rechipe.forEach(element =>{
-        Option +=`
+    var Option = "";
+    rechipe.forEach(element => {
+        Option += `
             <option value = "${element.id}">${element.name}</option>
         `;
     });
     $('#recipe').append(Option);
 }
-function getRecipe(rechipeId){
-    alldata.forEach(element =>{
-            if(element.id == rechipeId){
-               getEachRecipe(element.name,element.iconUrl);
-               eachIngredient(element.ingredients);
-               eachStep(element.instructions);
-            }
-        
-        
-    });   
+
+// getRecipe
+function getRecipe(rechipeId) {
+    alldata.forEach(element => {
+        if (element.id == rechipeId) {
+            getEachRecipe(element.name, element.iconUrl);
+           
+            eachIngredient(element.ingredients);
+            eachStep(element.instructions);
+            numberGuest(element.nbGuests);
+            oldGuest = element.nbGuests;
+            oldQuantity = element;
+           
+        }
+    });
 }
 
+
+
+
+
 /////  Get iconUrl and Name of food
-var getEachRecipe = (name,img) =>{
+var getEachRecipe = (name, img) => {
     var result = "";
-    result +=`
+    result += `
         <h3>${name}</h3> 
     `;
     $('#card').html(result);
-   
+
     var results = "";
-    results +=`
-        <img src="${img}" width="100"> 
+    results += `
+        <img src="${img}" width="300" class="img-thumbnail"> 
     `;
     $('#cards').html(results);
 }
@@ -65,39 +128,82 @@ var getEachRecipe = (name,img) =>{
 
 /// Get ingredients
 
+var oldQuantity ;
 function eachIngredient(ingredient) {
     var ing = "";
+    
     ingredient.forEach(el => {
-        ing +=`
+        ing += `
             <tr>
               <td><img src="${el.iconUrl}" width="100"></td>
-                <td>${el.name}</td>
+               
                 <td>${el.quantity}</td>
                 <td>${el.unit[0]}</td>
+                <td>${el.name}</td>
                 
             </tr>
         `;
     });
-    $('#table').html(ing);
+    $('#ingre').html("Ingredients");
+    $('#ingredient').html(ing);
 }
 
 
 
-// get instruction
+// get instructions
 function eachStep(instruction) {
-    var result ="";
+    var result = "";
     var ing = instruction.split('<step>');
-    for(var i = 1; i < ing.length; i++) {
-        result +=`
+    for (var i = 1; i < ing.length; i++) {
+        result += `
             <h5 class="text-primary">Step:${i}</h5>
             <p>${ing[i]}</p>
         `;
     }
+    $('#instruct').html("Instruction");
     $('#instruction').html(result);
-   
-   
+
+
 }
 
+
+// Get number of guests
+function numberGuest(guest) {
+   
+    var result = "";
+    result += `
+    <input type="text" class="form-control text-center" value="${guest}" id="member" disabled>
+    `;
+
+    $('#input').html(result);
+} 
+
+
+//Add person
+
+function addPerson(adds)  {
+    var added = parseInt(adds) +1;
+    if(added<=15) {
+        $('#member').val(added);
+        calculate($("#member").val());
+      
+    }
+}
+
+// Decrease person
+function decreasePerson(decrease)  {
+    var decrease = parseInt(decrease)-1;
+    if(decrease>0) {
+        $('#member').val(decrease);
+        calculate($("#member").val());
+        
+    }
+}
+
+
+
+
+  
 
 
 
